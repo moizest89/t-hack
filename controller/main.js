55e86aae3ff0c44ae4a21f6417f78b7359eab56b
moizest89 <moises.gilberto@gmail.com>
2013-05-26 14:25:44 -0600

    update_pages

--- controller/main.js
+++ controller/main.js
@@ -6,14 +6,5 @@ function LoginControl($scope) {
       console.log(data)
     });
   
-    // jQuery.ajax({
-    //     url: "http://108.166.64.74:3000/users/sign_in.json",
-    //     data: $("#login_form").serialize(),
-    //     type: "POST",
-    //     //dataType: "json",
-    //     success: function(data){
-    //       console.log(data)
-    //     }
-    // });
   }
 }
