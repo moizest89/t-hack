75322bb5eadc024ebce343453a66454849af476a
carcmonge <carcmonge@gmail.com>
2013-05-26 06:24:20 -0600

    update

--- controller/main.js
+++ controller/main.js
@@ -1,5 +1,17 @@
-$(document).on("ready",function(){
-	$('#master_body').load('view/main/index.html', function(){
-		console.log("entro :D");
-	})
-});
+function LoginControl($scope) {
+  
+  $scope.entrar = function() {
+
+    //$.post("http://108.166.64.74:3000/users/sign_in", $("#login_form").serialize(),function(data){console.log(data)})
+  
+    jQuery.ajax({
+        url: "http://108.166.64.74:3000/users/sign_in?callback=?",
+        data: $("#login_form").serialize(),
+        type: "POST",
+        dataType: "json",
+        success: function(data){
+          alert(data)
+        }
+    });
+  }
+}
