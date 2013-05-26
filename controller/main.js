f08179d29f12d3b8a6001ca38f5689a566bf7de4
carcmonge <carcmonge@gmail.com>
2013-05-26 07:28:35 -0600

    update

--- controller/main.js
+++ controller/main.js
@@ -2,16 +2,7 @@ function LoginControl($scope) {
   
   $scope.entrar = function() {
 
-    //$.post("http://108.166.64.74:3000/users/sign_in", $("#login_form").serialize(),function(data){console.log(data)})
-  
-    jQuery.ajax({
-        url: "http://108.166.64.74:3000/users/sign_in?callback=?",
-        data: $("#login_form").serialize(),
-        type: "POST",
-        dataType: "json",
-        success: function(data){
-          alert(data)
-        }
-    });
+    $.post("http://108.166.64.74:3000/users/sign_in.json", $("#login_form").serialize(),function(data){console.log(data)})
   }
+
 }
