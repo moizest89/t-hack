697df47dfd1a4a11f0bbfb4420ff4b2595719931
moizest89 <moises.gilberto@gmail.com>
2013-05-26 03:25:57 -0600

    update_merge

---
+++ assets/js/main_script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	 height_form_index();
+});
+
+$(window).resize(function(){
+	height_form_index();
+});
+
+
+function height_form_index () {
+	var height_page = $(document).height(); 
+	var width_page = $(window).width(); 
+	$('span#page_center,span#presentation_logo').css({
+		width:width_page,
+		height:height_page
+	});
+}
