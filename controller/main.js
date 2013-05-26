90f842ea91edbebbd4096232375e1601e1d69fb1
moizest89 <moises.gilberto@gmail.com>
2013-05-26 07:25:10 -0600

    update 2

--- controller/main.js
+++ controller/main.js
@@ -2,7 +2,9 @@ function LoginControl($scope) {
   
   $scope.entrar = function() {
 
-    $.post("http://108.166.64.74:3000/users/sign_in.json", $("#login_form").serialize(),function(data){console.log(data)})
+    $.post("http://108.166.64.74:3000/users/sign_in.json", $("#login_form").serialize(),function(data){
+      console.log(data)
+    });
   
     // jQuery.ajax({
     //     url: "http://108.166.64.74:3000/users/sign_in.json",
